aaf209c98f220c2983fb8789fcf4cb2402b2eea7
renegadik <ilya.ievlev14@gmail.com>
2025-11-06 18:53:51 +0300

    add lis basic

---
+++ dynamic_programming/lis/lis_basic.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n, 0);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    std::vector<int> dp(n, 1);
+    std::vector<int> p(n, -1);
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i]) {
+                if (dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    p[i] = j;
+                }
+            }
+        }
+    }
+
+    int best_len = 0;
+    int best_pos = 0;
+    for (int i = 0; i < n; i++) {
+        if (dp[i] > best_len) {
+            best_len = dp[i];
+            best_pos = i;
+        }
+    }
+
+    std::vector<int> answer;
+    int cur = best_pos;
+
+    while (cur != -1) {
+        answer.push_back(a[cur]);
+        cur = p[cur];
+    }
+
+    for (int i = answer.size() - 1; i >= 0; i--) {
+        std::cout << answer[i] << " ";
+    }
+    std::cout << std::endl;
+}
